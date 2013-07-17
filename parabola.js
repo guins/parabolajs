aaa8b7a589e31528c3d5a1484dfc855543f9b285
Stéphane Guigné <stephane.guigne@invokemedia.com>
2013-07-17 10:36:35 -0700

    add comments

--- parabola.js
+++ parabola.js
@@ -1,13 +1,38 @@
+/*
+* Parabola JS
+* Javascript helper class providing useful parabola methods
+* 
+* @author Stéphane Guigné (http://stephaneguigne.com)
+* 
+* @url https://github.com/guins/parabolajs
+* @license MIT
+*
+* @date July 2013
+*/
 (function(w, undefined)
 {
     'use strict';
 
     if( w.Parabola ) return;
 
+    /*
+    * Parabola Class
+    *
+    * @coefficient (object) - Object containing the 3 Parabola coefficients(a, b, c).
+    *
+    * @return Parabola Instance
+    */
     w.Parabola = function(coefficient){
 
         var coeff = coefficient;
 
+        /**
+         * Get Point coordinates on the parabola at x value on the abscissa
+         * 
+         * @param  {int} x - Point position on the abscissa
+         * 
+         * @return {object} - Point coordinates(x, y) on the parabola
+         */
         function getCoordinates(x){
             return {
                 x : x,
@@ -15,6 +40,14 @@
             };
         }
 
+        /**
+         * Get Quadratic Point between two points on the parabola
+         * 
+         * @param  {object} p1 - first Point(x,y) coordinates
+         * @param  {object} p2 - second Point(x,y) coordinates
+         * 
+         * @return {object} - Quadratic Point coordinates(x, y)
+         */
         function getQuadraticPoint(p1, p2)
         {
             var x1 = p1.x,
@@ -26,6 +59,11 @@
             };
         }
 
+        /**
+         * Get Coefficient (Getter)
+         * 
+         * @return {object} - Parabola cofficients(a, b, c)
+         */
         function getCoefficient(){
             return coeff;
         }
